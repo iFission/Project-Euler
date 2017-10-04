788daa0cd129f20be26b6d552f772bd1ecc0818c
iFission <WEIJIE@ALEX-PC.470712>
2017-10-04 22:00:21 +0800

    divided prime

--- 3.py
+++ 3.py
@@ -12,29 +12,9 @@ def main():
 		# print("x is: {x}".format(x=x))
 		if n % x == 0 and is_prime(x) and f < x:
 			f = x
-<<<<<<< HEAD
-<<<<<<< HEAD
 			n = int(n / x)
-			# print(n)
-=======
-
-			if is_prime(int(n / x)) and f < x: # check the complementary factor is prime
-				f = x
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 1473b44... sieve with sqrt, complementary
-=======
->>>>>>> parent of 1473b44... sieve with sqrt, complementary
-=======
->>>>>>> parent of 1473b44... sieve with sqrt, complementary
-=======
->>>>>>> parent of 1473b44... sieve with sqrt, complementary
-=======
-			n = int(n / x)
-			# print(n)
->>>>>>> master
+			# if is_prime(int(n / x)) and f < x: # check the complementary factor is prime
+			# 	f = x
 			print(f)
 
 def is_prime(n): # ensure n only has itself as a factor
