ebd4ac5ed955dfe1ea4eaad0a262d9da395c9788
iFission <WEIJIE@ALEX-PC.470712>
2017-10-06 18:37:31 +0800

    problem 3 update for last prime

--- 3.py
+++ 3.py
@@ -5,10 +5,12 @@
 import math as m
 
 def main():
-	n = 600851475143
+	n = 6008514751435
 	f = 0
+	x = 2
 
-	for x in range(3, int(m.sqrt(n))): # stop at the square root of n
+	# for x in range(3, int(m.sqrt(n))): # stop at the square root of n
+	while x < int(m.sqrt(n)): # allows for update of range as n changes
 		# print("x is: {x}".format(x=x))
 		if n % x == 0 and is_prime(x) and f < x:
 			f = x
@@ -16,8 +18,13 @@ def main():
 			# if is_prime(int(n / x)) and f < x: # check the complementary factor is prime
 			# 	f = x
 			print(f)
+		x += 1
+	if n == 1:
+		print(f) # as n will have at most 1 prime factor greater than sqrt(n), by doing prime fac, the last factor will be factored n, which is prime
+	else:
+		print(n) # accounts for 1 and 2
 
-def is_prime(n): # ensure n only has itself as a factor
+def is_prime(n): # ensure n only has itself as a factor, assumes 2 is prime
 	for x in range(2, n - 1): # start at 2, skip 1, stop at n-1, hence min n = 3
 		# print("x is: {x} for {n}".format(x=x, n=n))
 		if n % x == 0:
