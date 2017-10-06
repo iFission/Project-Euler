323b7b844a83791af1f63c7f2bb60a5c8d4740d1
iFission <WEIJIE@ALEX-PC.470712>
2017-10-06 19:14:08 +0800

    3 final

--- 3.py
+++ 3.py
@@ -5,24 +5,25 @@
 import math as m
 
 def main():
-	n = 6008514751435
-	f = 0
+	n = 600851475143
+	f = 1 # accounts for 1
 	x = 2
 
 	# for x in range(3, int(m.sqrt(n))): # stop at the square root of n
-	while x < int(m.sqrt(n)): # allows for update of range as n changes
+	while x <= int(m.sqrt(n)): # allows for update of range as n changes, <= accounts for perfect squares
 		# print("x is: {x}".format(x=x))
-		if n % x == 0 and is_prime(x) and f < x:
+		while n % x == 0: # doesnt have to check for prime, while checks for repeated factors
 			f = x
-			n = int(n / x)
-			# if is_prime(int(n / x)) and f < x: # check the complementary factor is prime
-			# 	f = x
-			print(f)
+			n = n / x
+			print("factor is: {f}".format(f=f))
+			if n == 1: # end when n has been fully factorised by factors <= sqrt(n), account for perfect squares
+				return
+
 		x += 1
 	if n == 1:
-		print(f) # as n will have at most 1 prime factor greater than sqrt(n), by doing prime fac, the last factor will be factored n, which is prime
+		print("factor is {f}".format(f=f)) # accounts for 1
 	else:
-		print(n) # accounts for 1 and 2
+		print("factor is: {f}".format(f=int(n))) # as n will have at most 1 prime factor greater than sqrt(n), by doing prime fac, the last factor will be factored n, which is prime # accounts for 2
 
 def is_prime(n): # ensure n only has itself as a factor, assumes 2 is prime
 	for x in range(2, n - 1): # start at 2, skip 1, stop at n-1, hence min n = 3
